949757abb3da9cd29856952d5f89011d77c0f78a
dubey <dubeyseema04@gmail.com>
2021-05-06 22:07:55 +0530

    click count

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get reference to button
+        val btnClickMe = findViewById<Button>(R.id.btn_click_me)
+        val textView = findViewById<TextView>(R.id.text_view)
+        var timesClicked = 0
+        // set on-click listener
+        btnClickMe.setOnClickListener {
+            timesClicked += 1
+            //or   timesClicked = timesClicked+1
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+            textView.text = timesClicked.toString()
+        }
     }
 }
